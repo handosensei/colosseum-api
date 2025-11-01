26cddb4340c88a01d765d045a3f11ab83441cbf0
Hando Masahashi <handosensei@gmail.com>
2025-11-02 00:18:28 +0100

    init cloudflare webhook

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudflareStreamService } from './service/cloudflare-stream.service';
+import { CloudflareWebhookController } from './controllers/cloudflare-webhook.controller';
 
 @Module({
   providers: [CloudflareStreamService],
   exports: [CloudflareStreamService],
+  controllers: [CloudflareWebhookController],
 })
 export class ClientModule {}
